305b75bf2b14428f0f9977019e8d2a7344c36866
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2019-02-04 16:53:51 +0900

    Added .env.template

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  const s = require('../server/spreadsheet');
-  s.addPayer("Ogi");
-
   res.render('index', { title: 'Express' });
+});
+
+/* Payment */
+router.post('/charge', async (req, res, next) => {
+	let {token,email,description} = req.body;
+
+  try {
+    let charge = await payment.charge(token,description,email);
+      
+    return res.status(200).json({charge});
+  } catch (err) {
+    return res.status(500).json({type: err.type, message: err.message});
+  }
 });
 
 module.exports = router;
